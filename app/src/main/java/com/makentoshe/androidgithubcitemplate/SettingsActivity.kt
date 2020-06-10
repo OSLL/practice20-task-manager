716163944f56c02f2f29f7701cd305fec046f1cd
Vasya <vasgor@users.noreply.github.com>
2020-06-10 21:33:30 +0300

    - Add Settings menu - Add Settings button in Main activity

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -0,0 +1,31 @@
+package com.makentoshe.androidgithubcitemplate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val actionBar = supportActionBar
+        actionBar?.title = "Profile"
+        actionBar?.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_settings)
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+    override fun onSupportNavigateUp():Boolean {
+        finish()
+        return true
+    }
+}
