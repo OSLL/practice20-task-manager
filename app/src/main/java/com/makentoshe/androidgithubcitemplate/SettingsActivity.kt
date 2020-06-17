84fb15f197e5f307521d3c145b9ae7b0fffae5c3
Vasya <vasgor@users.noreply.github.com>
2020-06-17 12:00:24 +0300

    - Add one working switch that changes theme for itself

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/SettingsActivity.kt
@@ -7,6 +7,7 @@ import android.widget.CompoundButton
 import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 class SettingsActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.AppTheme_Dark)
+        } else { setTheme(R.style.AppTheme) }
+
         super.onCreate(savedInstanceState)
         val actionBar = supportActionBar
         actionBar?.title = "Profile"
@@ -68,11 +74,20 @@ class SettingsActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeList
         Toast.makeText(this, "The Switch is " + (if (isChecked) "on" else "off"), Toast.LENGTH_SHORT).show()
         if(isChecked) {
             //do stuff when Switch is ON
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            restartApp()
         } else {
             //do stuff when Switch if OFF
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            restartApp()
         }
     }
 
+    fun restartApp() {
+        intent = Intent(applicationContext, SettingsActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         intent = Intent(this, MainActivity::class.java)
