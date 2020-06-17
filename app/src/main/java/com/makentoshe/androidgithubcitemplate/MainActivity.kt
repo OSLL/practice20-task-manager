83c54453f261e33afc78dd793d5289fe04d4e68e
Ildar <ildargainanov@ya.ru>
2020-06-17 22:12:53 +0300

    - Edit: separate Callback and MainActivity

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.makentoshe.androidgithubcitemplate
 
-import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,61 +13,9 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.bottom_navigation
-import kotlinx.android.synthetic.main.activity_stats.*
 
-interface AppCallback {
-    fun updateAdapter(groupAdapter: GroupAdapter<ViewHolder>, db: TaskDatabase, deleteId: Long = -1){
-    }
 
-    class SwipeCallback(
-        private var appCallback: AppCallback,
-        adapter: GroupAdapter<ViewHolder>,
-        application: Application,
-        private var db: TaskDatabase
-    ) : AppCallback,
-        ItemTouchHelper.SimpleCallback(0, 0) {
-        val app = application
-        private fun removeItem (pos: Int, adapter: GroupAdapter<ViewHolder>) {
-        }
 
-        private val mAdapter: GroupAdapter<ViewHolder> = adapter
-        override fun getMovementFlags(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder
-        ): Int {
-            val pos = viewHolder.adapterPosition
-            return ItemTouchHelper.Callback.makeMovementFlags(
-                createDragFlags(pos),
-                createSwipeFlags(pos)
-            )
-        }
-
-        private fun createDragFlags(pos: Int): Int {
-            return if (pos == 0) 0 else ItemTouchHelper.UP or ItemTouchHelper.DOWN
-        }
-
-        private fun createSwipeFlags(pos: Int): Int {
-            return if (pos == 0) 0 else ItemTouchHelper.START or ItemTouchHelper.END
-        }
-
-        //This method is not needed, hence return false
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ): Boolean {
-            return false
-        }
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            var taskDao = db.taskDao()
-            val position: Long = taskDao.getAll()[0].id-1+viewHolder.adapterPosition.toLong()
-            Log.v("Clr", position.toString())
-            appCallback.updateAdapter(mAdapter, db, position)
-        }
-
-    }
-
-}
 class MainActivity : AppCompatActivity(), AppCallback {
     var groupAdapter = GroupAdapter<ViewHolder>()
     override fun updateAdapter(groupAdapter: GroupAdapter<ViewHolder>, db: TaskDatabase, deleteId: Long) {
