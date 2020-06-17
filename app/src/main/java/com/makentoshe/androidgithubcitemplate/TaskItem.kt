922a31c5acece4bd4fc0eed7782e8b7d848677ae
Ildar <ildargainanov@ya.ru>
2020-06-17 12:02:08 +0300

    Merge branch 'BetaBranch' of D:\Users\Ildar.DESKTOP-3FQ02FC\AndroidStudioProjects\practice20-task-manager-2 with conflicts.

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/TaskItem.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/TaskItem.kt
@@ -1,19 +1,26 @@
-package com.makentoshe.androidgithubcitemplate
+package com.makentoshe.androidgithubcitemplate.items
 
 import android.view.View
+import com.makentoshe.androidgithubcitemplate.INSET
+import com.makentoshe.androidgithubcitemplate.INSET_TYPE_KEY
+import com.makentoshe.androidgithubcitemplate.R
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.note_layout.*
 
-class TaskItem(private val task: Task) : Item() {
-    override fun getLayout() = R.layout.note_layout
-
+open class TaskItem(private val task: Task) : Item() {
+    //open val swipeDirs: Int = 0
+    init {
+        extras[INSET_TYPE_KEY] = INSET
+    }
+    override fun getLayout() =
+        R.layout.note_layout
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.apply {
             note_title.text = task.title
             if (task.text != "") note_text.visibility = View.VISIBLE
             note_text.text = task.text
-            if (task.date.time > 0) {
+            if (task.date > 0) {
                 alarm_icon.visibility = View.VISIBLE
                 note_date.visibility = View.VISIBLE
                 note_date.text = "High Noon"
