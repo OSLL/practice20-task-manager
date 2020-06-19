5137a24964bc2b3abbe37a9613f15028847ab0da
Ildar <ildargainanov@ya.ru>
2020-06-19 11:45:13 +0300

    - Card fixes

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/items/Rating.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/items/Rating.kt
@@ -1,10 +1,18 @@
 package com.makentoshe.androidgithubcitemplate.items
 
 import android.content.SharedPreferences
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.*
 
-class Rating(
-    var Rating: Int = 100,
+@Entity
+class Rating constructor(
+    var Rating: Int = 50,
     var SolvedToday: Int = 0,
     var RejectedToday: Int = 0,
-    var pref: SharedPreferences
-) : TaskNote
+    var pref: SharedPreferences,
+    @PrimaryKey
+    var id: Long = 0
+) : TaskNote {}
